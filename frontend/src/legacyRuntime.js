0d8760e8ade704f5b55933af11dae692f0364c2e
andreyabramovworks-commits <andreyabramovworks@gmail.com>
2026-08-11 16:32:34 +0300

    release: v52.0.2 run versioned deploy script

--- frontend/src/legacyRuntime.js
+++ frontend/src/legacyRuntime.js
@@ -1,5 +1,5 @@
-export const RELEASE_VERSION = "52.0.1";
-export const RELEASE_ASSET_REVISION = "52.0.1-r1";
+export const RELEASE_VERSION = "52.0.2";
+export const RELEASE_ASSET_REVISION = "52.0.2-r1";
 
 export const LEGACY_STYLES = [
   "/legacy/runtime-core.css",
